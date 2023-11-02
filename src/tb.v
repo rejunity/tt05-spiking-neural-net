3d468a4b4ed2045e026bd12986d0684ac648eec3
rej <rzioma@acm.org>
2023-11-02 18:41:03 +0100

    Cleanup

--- src/tb.v
+++ src/tb.v
@@ -22,8 +22,6 @@ module tb ();
     reg  ena;
     reg  [7:0] ui_in;
     reg  [7:0] uio_in;
-
-    wire [6:0] segments = uo_out[6:0];
     wire [7:0] uo_out;
     wire [7:0] uio_out;
     wire [7:0] uio_oe;
